5a5c114d717e36a6151b755c1f1a7fd7fa0ea53b
Roberto Junior <140918030+Robertopradojr@users.noreply.github.com>
2023-08-06 00:18:25 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+function login(){
+    var h = window.document.getElementsByClassName('.formulariologin')
+}
+
+function start(){
+    var h = window.document.getElementById('nome1')
+    var i = window.document.getElementById('capsula')
+    h.innerHTML = 'Start'
+}
+function saiu(){
+    var h = window.document.getElementById('nome1')
+    h.innerHTML = 'Camelot'
+}
